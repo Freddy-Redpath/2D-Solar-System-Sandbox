9ce5cf2aee8a07ee644200ac0edbcb0831a22b74
Maadhyam Rawal <maadhyam.rawal@student.manchester.ac.uk>
2025-03-27 16:06:00 +0000

    Merge remote-tracking branch 'origin/Physics-Dev' into Physics-Dev

--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -22,48 +22,48 @@ public class SolarSystem {
         addStar(theSun);
     }
 
-    public void addEarth(){
-        Planet earth = new Planet(152.1, 0.0, 5.97e24, 29295.1810, 0.0,152.1,0.017,365.2,0.0,0.0,152.1, 12756.0 , "src/images/Earth.png", "earth");
-        addPlanet(earth);
-    }
-
     public void addMercury() {
-        Planet mercury = new Planet(69.8, 0.0, 0.330e24, 38864.7570, 0.0, 69.8, 0.206, 87.97, 0.0, 0.0, 69.8, 4879.0, "src/images/Mercury.png", "mercury");
+        Planet mercury = new Planet(69.8E9, 0.0, 0.330e24, 38864.7570, 0.0, 69.8E9, 0.206, 7603200.0, 0.0, 0.0, 57910000.0, 4879.0, "src/images/Mercury.png", "mercury");
         addPlanet(mercury);
     }
 
     public void addVenus() {
-        Planet venus = new Planet(108.9, 0.0, 4.87e24, 34790.1818, 0.0, 108.9, 0.007, 224.7, 0.0, 0.0, 108.9, 12104.0, "src/images/Venus.png", "venus");
+        Planet venus = new Planet(108.9E9, 0.0, 4.87e24, 34790.1818, 0.0, 108.9E9, 0.007, 19408080.0, 0.0, 0.0, 108200000.0, 12104.0, "src/images/Venus.png", "venus");
         addPlanet(venus);
     }
 
+    public void addEarth(){
+        Planet earth = new Planet(152.1E9, 0.0, 5.97e24, 29295.1810, 0.0,152.1E9,0.017,31557280.0,0.0,0.0,149600000.0, 12756.0 , "src/images/Earth.png", "earth");
+        addPlanet(earth);
+    }
+
     public void addMars() {
-        Planet mars = new Planet(249.3, 0.0, 0.642e24, 21974.9314, 0.0, 249.3, 0.093, 687.0, 0.0, 0.0, 249.3, 6792.0, "src/images/Mars.png", "mars");
+        Planet mars = new Planet(249.3E9, 0.0, 0.642e24, 21974.9314, 0.0, 249.3E9, 0.093, 2359200.0, 0.0, 0.0, 227900000.0, 6792.0, "src/images/Mars.png", "mars");
         addPlanet(mars);
     }
 
     public void addJupiter() {
-        Planet jupiter = new Planet(816.4, 0.0, 1898e24, 12434.5425, 0.0, 816.4, 0.049, 4331, 0.0, 0.0, 816.4, 142984.0, "src/images/Jupiter.png", "jupiter");
+        Planet jupiter = new Planet(816.4E9, 0.0, 1898e24, 12434.5425, 0.0, 816.4E9, 0.049, 59356800.0, 0.0, 0.0, 778500000.0, 142984.0, "src/images/Jupiter.png", "jupiter");
         addPlanet(jupiter);
     }
 
     public void addSaturn() {
-        Planet saturn = new Planet(1506.5, 0.0, 568e24, 9100.3757, 0.0, 1506.5, 0.056, 10747, 0.0, 0.0, 1506.5, 120536.0, "src/images/Saturn.png", "saturn");
+        Planet saturn = new Planet(1506.5E9, 0.0, 568e24, 9100.3757, 0.0, 1506.5E9, 0.056, 374942400.0, 0.0, 0.0, 1427000000.0, 120536.0, "src/images/Saturn.png", "saturn");
         addPlanet(saturn);
     }
 
     public void addUranus() {
-        Planet uranus = new Planet(3001.4, 0.0, 86.8e24, 6484.6825, 0.0, 3001.4, 0.046, 30589, 0.0, 0.0, 3001.4, 51118.0, "src/images/Uranus.png", "uranus");
+        Planet uranus = new Planet(3001.4E9, 0.0, 86.8e24, 6484.6825, 0.0, 3001.4E9, 0.046, 928540800.0, 0.0, 0.0, 2871000000.0, 51118.0, "src/images/Uranus.png", "uranus");
         addPlanet(uranus);
     }
 
     public void addNeptune() {
-        Planet neptune = new Planet(4558.9, 0.0, 102e24, 5383.9657, 0.0, 4558.9, 0.010, 59800, 0.0, 0.0, 4458.9, 49528.0, "src/images/Neptune.png", "neptune");
+        Planet neptune = new Planet(4558.9E9, 0.0, 102e24, 5383.9657, 0.0, 4558.9E9, 0.010, 2642889600.0, 0.0, 0.0, 4495000000.0, 49528.0, "src/images/Neptune.png", "neptune");
         addPlanet(neptune);
     }
 
     public void addPluto() {
-        Planet pluto = new Planet(7375.9, 0.0, 0.0130e24, 3676.9789, 0.0, 7375.9, 0.248, 90560, 0.0, 0.0, 7375.9, 2376.0, "src/images/Pluto.png", "pluto");
+        Planet pluto = new Planet(7375.9E9, 0.0, 0.0130e24, 3676.9789, 0.0, 7375.9E9, 0.248, 5167920000.0, 0.0, 0.0, 5906000000.0, 2376.0, "src/images/Pluto.png", "pluto");
         addPlanet(pluto);
     }
 
