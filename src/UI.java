cdc20b76724417452fc85296458e8085f55eadb5
Zaid <zaid.setaboha@gmail.com>
2025-03-27 19:53:20 +0000

    Buttons get darker when hovered over

--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 
 public class UI extends JPanel {
 
@@ -17,15 +18,45 @@ public class UI extends JPanel {
       window.setLocationRelativeTo(null);
       window.setVisible(true);
 
+      JSlider slider = SliderCreator(0, 300, 300, 100, 300, 50);
+      window.add(slider);
+      window.setLocationRelativeTo(null);
+      window.setVisible(true);
+
+
    }
    public JButton ButtonCreator( Color colour, int xPos, int yPos, int width, int height, String text){
       JButton button = new JButton(text);
       button.setBackground(colour);
       //changeColour.setForeground( new Color(153,153,252));
       button.setBounds(xPos, yPos, width, height);
+
+      Color Og_color = colour;
+      Color new_color = colour.darker();
+
+      button.addMouseListener(new MouseAdapter() {
+
+         public void mouseEntered(MouseEvent e) {
+            button.setBackground(new_color);
+         }
+         public void mouseExited(MouseEvent e) {
+            button.setBackground(Og_color);
+         }
+
+      });
+
       button.addActionListener(e -> {
 
       });
       return button;
    }
+   }
+   public JSlider SliderCreator(int min, int max, int xPos, int yPos, int width, int height){
+      JSlider slider = new JSlider(JSlider.HORIZONTAL, min, max, (min + max) / 2);
+      slider.setBounds(xPos, yPos, width, height);
+      slider.setMajorTickSpacing(50);
+      slider.setMinorTickSpacing(10);
+      slider.setPaintTicks(true);
+      slider.setPaintLabels(true);
+
    }
